3cc24ffb0381e5496954e49146b2ad6753cec4c6
asb <asbabushkin@yandex.ru>
2022-08-23 17:47:10 +0500

    model SearchResult added

--- flight_catcher/flight_search/models.py
+++ flight_catcher/flight_search/models.py
@@ -6,9 +6,15 @@ class Traveller(models.Model):
     phone_num = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.telegr_acc
+
 
 class SeatClass(models.Model):
     name = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Search(models.Model):
@@ -22,7 +28,19 @@ class Search(models.Model):
     num_infants = models.SmallIntegerField()
     num_childs = models.SmallIntegerField()
     luggage = models.BooleanField(default=False)
-    seat_class = models.ForeignKey(SeatClass, on_delete=models.SET_NULL)
+    seat_class = models.ForeignKey(SeatClass, null=True, on_delete=models.SET_NULL)
     search_init_date = models.DateTimeField(auto_now_add=True)
     search_until_date = models.DateTimeField(null=False)
-    user_id = models.ForeignKey(Traveller, on_delete=models.SET_NULL)
+    user_id = models.ForeignKey(Traveller, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.depature_city} - {self.dest_city}'
+
+class SearchResult(models.Model):
+    search_id = models.ForeignKey(Search, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    link = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'search {self.search_id}'
