2ea7ab52e08a436dc2fc7218cd6d6e4bd931745f
asb <asbabushkin@yandex.ru>
2022-08-23 21:21:42 +0500

    index.html, base.html created

--- flight_catcher/flight_search/views.py
+++ flight_catcher/flight_search/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+from .models import *
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Flight cather start page')
+    seatclass = SeatClass.objects.all()
+    return render(request, 'flight_search/index.html', {'title': 'Flight catcher', 'seatclass': seatclass})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена<h1>')
