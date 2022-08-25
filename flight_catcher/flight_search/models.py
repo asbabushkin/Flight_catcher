8ca14fa37cff41d691e0e75d3e62a231b50ad768
asb <asbabushkin@yandex.ru>
2022-08-25 13:46:43 +0500

    models Traveller, SeatClass are deleted.

--- flight_catcher/flight_search/models.py
+++ flight_catcher/flight_search/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 
-class Traveller(models.Model):
-    telegr_acc = models.CharField(max_length=50)
-    phone_num = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.telegr_acc
-
-
-class SeatClass(models.Model):
-    name = models.CharField(max_length=20, null=False)
-
-    def __str__(self):
-        return self.name
-
-
 class Search(models.Model):
     depature_city = models.CharField(max_length=50)
     dest_city = models.CharField(max_length=50)
@@ -28,10 +12,12 @@ class Search(models.Model):
     num_infants = models.SmallIntegerField()
     num_childs = models.SmallIntegerField()
     luggage = models.BooleanField(default=False)
-    seat_class = models.ForeignKey(SeatClass, null=True, on_delete=models.SET_NULL)
     search_init_date = models.DateTimeField(auto_now_add=True)
     search_until_date = models.DateTimeField(null=False)
-    user_id = models.ForeignKey(Traveller, null=True, on_delete=models.SET_NULL)
+    telegr_acc = models.CharField(max_length=50)
+    phone_num = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+
 
     def __str__(self):
         return f'{self.depature_city} - {self.dest_city}'
