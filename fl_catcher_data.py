87e91cb3d0d2994c71889eb29f5f70bdb2b4dbe6
asb <asbabushkin@yandex.ru>
2022-08-28 22:03:57 +0500

    flight_parser.py, tg_bot, fl_catcher_data.py added to the project

---
+++ fl_catcher_data.py
@@ -0,0 +1,6 @@
+telegram_api = 18922333
+telegram_hash = 'e9d01bc703f33f1586c65f14f85d14b5'
+token = '5767913217:AAHrAHPrMabYIZX4ZPtQ5OcTjJ8HEaUFA1o'
+telegram_user_name = '@asbabushkin'
+url_ottrip = 'https://www.onetwotrip.com/ru/f/search/2908CEKLED?s=true&sc=E&ac=1&tr=0'
+telegram_user_id = '1506995678'
