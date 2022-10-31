d80bb2b2077aaa451e8cc1a9a596094b801c6f25
asb <asbabushkin@yandex.ru>
2022-10-31 11:01:37 +0500

    Validators for Search form created.

--- flight_catcher/flight_search/forms.py
+++ flight_catcher/flight_search/forms.py
@@ -1,19 +1,19 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.forms import TextInput, DateTimeInput, NumberInput, DateInput, DateField
-
+import datetime
 from flight_catcher import settings
 from .models import Search, CityCode
-#from .widgets import DatePickerInput
-import datetime
+
+
+# from .widgets import DatePickerInput
 
 
 class SearchForm(forms.ModelForm):
-
     class Meta:
         model = Search
         fields = ['depature_city', 'dest_city', 'max_transhipments', 'depart_date', 'return_date',
-                  'num_adults', 'num_children', 'luggage', 'telegr_acc',]
+                  'num_adults', 'num_children', 'luggage', 'telegr_acc', ]
 
         widgets = {
             'depature_city': TextInput(attrs={
@@ -72,9 +72,6 @@ class SearchForm(forms.ModelForm):
         depature_city = self.cleaned_data['depature_city']
         if not CityCode.objects.filter(city_rus=depature_city):
             raise ValidationError('Город вылета не найден!')
-
-        if len(depature_city) > 40:
-            raise ValidationError('Длина превышает 40 символов')
         return depature_city
 
     def clean_dest_city(self):
@@ -83,10 +80,39 @@ class SearchForm(forms.ModelForm):
             raise ValidationError('Город назначения не найден!')
         return dest_city
 
+    def clean_depart_date(self):
+        if self.cleaned_data['depart_date'] < datetime.date.today():
+            raise ValidationError('Дата вылета уже прошла.')
+        return self.cleaned_data['depart_date']
+
+    def clean_return_date(self):
+        if self.cleaned_data['return_date'] is not None:
+            if self.cleaned_data['return_date'] < datetime.date.today():
+                raise ValidationError('Дата возвращения уже прошла.')
+            if self.cleaned_data['return_date'] < self.cleaned_data['depart_date']:
+                raise ValidationError('Дата возвращения ранее даты вылета.')
+
+        return self.cleaned_data['return_date']
+
+    def clean_max_transhipments(self):
+        if 0 > self.cleaned_data['max_transhipments'] > 3:
+            raise ValidationError('От 0 до 3 пересадок')
+        return self.cleaned_data['max_transhipments']
+
+    def clean_num_adults(self):
+        if self.cleaned_data['num_adults'] < 1 or self.cleaned_data['num_adults'] > 10:
+            raise ValidationError('Не менее 1 и не более 10 взрослых пассажиров.')
+        return self.cleaned_data['num_adults']
+
     def clean_num_children(self):
-        if self.cleaned_data['num_children'] > 0 and self.cleaned_data['num_adults'] == 0:
-            raise ValidationError('Ребенок не может лететь без взрослого!')
+        if self.cleaned_data['num_children'] < 0 or self.cleaned_data['num_children'] > 10:
+            raise ValidationError('Некорректное значение в поле "Количество детей" (не более 10)')
         return self.cleaned_data['num_children']
+
+    def clean_telegr_acc(self):
+        if self.cleaned_data['telegr_acc'][0] != '@':
+            raise ValidationError('Телеграм-аккаунт должен начинаться со знака "@"')
+        return self.cleaned_data['telegr_acc']
 
     # depature = forms.CharField(min_length=2, max_length=50, strip=True, label='Откуда')
     # dest = forms.CharField(min_length=2, max_length=50, strip=True, label='Куда')
