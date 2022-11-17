51bf62d97df1250c47822f18741c34f24bf2c426
asb <asbabushkin@yandex.ru>
2022-11-17 15:25:56 +0500

    def test_redirects_to_success_page_on_success(self) fixed

--- flight_catcher/flight_search/tests/test_views.py
+++ flight_catcher/flight_search/tests/test_views.py
@@ -6,18 +6,33 @@ from flight_search.models import *
 
 class IndexPageTest(TestCase):
     def setUp(self):
+        cities = [{
+            'city_eng': 'Moscow',
+            'city_rus': 'Москва',
+            'code_eng': 'MSC',
+            'code_rus': 'МСК',
+        },
+            {
+                'city_eng': 'Sochi',
+                'city_rus': 'Сочи',
+                'code_eng': 'SCH',
+                'code_rus': 'СОЧ',
+            }
+        ]
+        for city in cities:
+            CityCode.objects.create(**city)
+
         self.form_data = {
             'depature_city': 'Москва',
-            'dest_city': 'Екатеринбург',
+            'dest_city': 'Сочи',
             'max_transhipments': 1,
             'depart_date': str(datetime.date.today() + datetime.timedelta(days=7)),
             #'return_date': None,
             'num_adults': 1,
             'num_children': 0,
-            #'luggage': False,
+            # 'luggage': False,
             'telegr_acc': '@my_account',
         }
-
 
 
 
@@ -31,33 +46,15 @@ class IndexPageTest(TestCase):
 
     def test_view_uses_correct_template(self):
         resp = self.client.get(reverse('home'))
-        # print(resp.context)
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'flight_search/index.html')
 
     def test_redirects_to_success_page_on_success(self):
-        # form_data = {
-        #     'depature_city': 'Москва',
-        #     'dest_city': 'Екатеринбург',
-        #     'max_transhipments': 1,
-        #     'depart_date': str(datetime.date.today() + datetime.timedelta(days=7)),
-        #     # 'return_date': None,
-        #     'num_adults': 1,
-        #     'num_children': 0,
-        #     # 'luggage': False,
-        #     'telegr_acc': '@my_account',
-        # }
-        # form = SearchForm(data=form_data)
         resp = self.client.post(reverse('home'), self.form_data)
-        print(resp.status_code)
-
-        #print(form.errors)
         self.assertRedirects(resp, reverse('search_result'))
-
 
     def test_form_invalid_depart_date_in_past(self):
         date_in_past = str(datetime.date.today() - datetime.timedelta(days=1))
-        #self.form_data['depart_date'] = date_in_past
         resp = self.client.post(reverse('home'), {'depart_date': date_in_past})
         self.assertEqual(resp.status_code, 200)
         self.assertFormError(resp, 'form', 'depart_date', 'Дата вылета уже прошла.')
