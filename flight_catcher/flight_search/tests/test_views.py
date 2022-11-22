ea4d91216d34d7d6ad8b04e5db43a205ccabf097
asb <asbabushkin@yandex.ru>
2022-11-22 14:50:17 +0500

    views tests created (class CommonPageTest) - test_views_use_correct_templates; - test_views_url_accessible_by_name; - test_views_url_exists_at_desired_location.

--- flight_catcher/flight_search/tests/test_views.py
+++ flight_catcher/flight_search/tests/test_views.py
@@ -32,14 +32,6 @@ class IndexPageTest(TestCase):
             'telegr_acc': '@my_account',
         }
 
-    def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('')
-        self.assertEqual(resp.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
-
     def test_redirects_to_success_page_on_success(self):
         resp = self.client.post(reverse('home'), self.form_data)
         self.assertRedirects(resp, reverse('search_result'))
@@ -49,18 +41,6 @@ class IndexPageTest(TestCase):
         resp = self.client.post(reverse('home'), {'depart_date': date_in_past})
         self.assertEqual(resp.status_code, 200)
         self.assertFormError(resp, 'form', 'depart_date', 'Дата вылета уже прошла.')
-
-    def test_views_use_correct_templates(self):
-        templates = {
-            'home': 'flight_search/index.html',
-            'search_result': 'flight_search/result.html',
-            'proj_descr': 'flight_search/project_description.html'
-        }
-        for key, expected_value in templates.items():
-            with self.subTest(key=key):
-                resp = self.client.get(reverse(key))
-                self.assertEqual(resp.status_code, 200)
-                self.assertTemplateUsed(resp, expected_value)
 
     def test_index_page_creates_correct_context(self):
         resp = self.client.get(reverse('home'))
@@ -81,12 +61,48 @@ class IndexPageTest(TestCase):
                 form_field = resp.context.get('form').fields.get(field_name)
                 self.assertIsInstance(form_field, expected_value)
 
-    def test_search_res_page_creates_correct_context(self):
+
+class ResultPageTest(TestCase):
+    def test_search_result_page_creates_correct_context(self):
         resp = self.client.get(reverse('search_result'))
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[-1]['title'], 'Запрос принят!')
 
+
+class ProjectDescriptionPageTest(TestCase):
     def test_proj_descr_page_creates_correct_context(self):
         resp = self.client.get(reverse('proj_descr'))
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.context[-1]['title'], 'Как это работает')
+
+
+class CommonPageTest(TestCase):
+    def test_views_use_correct_templates(self):
+        templates = {
+            'home': 'flight_search/index.html',
+            'search_result': 'flight_search/result.html',
+            'proj_descr': 'flight_search/project_description.html'
+        }
+        for key, expected_value in templates.items():
+            with self.subTest(key=key):
+                resp = self.client.get(reverse(key))
+                self.assertEqual(resp.status_code, 200)
+                self.assertTemplateUsed(resp, expected_value)
+
+    def test_views_url_accessible_by_name(self):
+        url_names = ['home', 'search_result', 'proj_descr']
+        for url_name in url_names:
+            with self.subTest(url_name=url_names):
+                resp = self.client.get(reverse(url_name))
+                self.assertEqual(resp.status_code, 200)
+
+    def test_views_url_exists_at_desired_location(self):
+        urls = {
+            'home': '',
+            'search_result': '/result/',
+            'proj_descr': '/project_description/',
+        }
+        for url_name, expected_value in urls.items():
+            with self.subTest(url_name=url_name):
+                resp = self.client.get(reverse(url_name))
+                self.assertEqual(resp.status_code, 200)
