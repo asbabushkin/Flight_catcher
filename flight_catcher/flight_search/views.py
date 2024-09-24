d7ef97814f43c44a4749313d7c59816739eab6ea
asb <asbabushkin@yandex.ru>
2024-09-24 23:34:35 +0500

    Update templates.

--- flight_catcher/flight_search/views.py
+++ flight_catcher/flight_search/views.py
@@ -18,7 +18,7 @@ def index(request):
         form = SearchForm()
 
     context = {
-        "title": "Мониторинг цен на авиабилеты",
+        "title": "Следит за ценой на авиабилеты",
         "form": form,
     }
     return render(request, "flight_search/index.html", context)
